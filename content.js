7bb4544748f6234dd9078aace3246b47fdb0db94
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2021-12-05 17:30:04 +0100

    Calc dislike to like percent and insert into dom

--- content.js
+++ content.js
@@ -1,8 +1,5 @@
+//'AIzaSyD80-B-IVojiKipon2pXqUd8uO5VCu4p84'
 const apiKey = 'AIzaSyD80-B-IVojiKipon2pXqUd8uO5VCu4p84'
-
-async function  getVideoInformation(callback) {
-    return await httpGetAsync(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${apiKey}`)
-}
 
 function httpGetAsync(theUrl, callback)
 {
@@ -11,7 +8,7 @@ function httpGetAsync(theUrl, callback)
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.open("GET", theUrl, true);
     xmlHttp.send(null);
 }
 
@@ -20,20 +17,30 @@ function youtube_parser(url){
     var match = url.match(regExp);
     return (match&&match[7].length==11)? match[7] : false;
 }
+function main() {
+    //Get video info from Youtube API
+    var videoId = youtube_parser(window.location.href);
+    httpGetAsync(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${apiKey}`,
+    (result) => {
+        resultParsed = JSON.parse(result);
+        let videoInfo = resultParsed.items[0].statistics;
 
-var videoId = youtube_parser(window.location.href);
-httpGetAsync(`https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${apiKey}`,
-(result) => {
-    resultParsed = JSON.parse(result);
-    let videoInfo = resultParsed.items[0].statistics;
+        //Set the dislike count
+        let likeElement = document.evaluate("//yt-formatted-string[contains(., 'Kan ikke lide')]", document, null, XPathResult.ANY_TYPE, null );
+        let ThisElement = likeElement.iterateNext();
+        ThisElement.innerHTML = videoInfo.dislikeCount;
 
-    let likeElement = document.evaluate("//yt-formatted-string[contains(., 'Kan ikke lide')]", document, null, XPathResult.ANY_TYPE, null );
-    let ThisElement = likeElement.iterateNext();
+        //Calculate the like to dislike percent
+        let likeDislikePercent = videoInfo.likeCount/(parseInt(videoInfo.likeCount) + parseInt(videoInfo.dislikeCount))*100;
 
-    ThisElement.innerHTML = videoInfo.dislikeCount;
-    let rationNumber = (videoInfo.likeCount/(videoInfo.likeCount + videoInfo.dislikeCount) * 100).toFixed(0);
-    var ratioElement = document.createElement("p");
-    var ratioText = document.createTextNode(rationNumber+ "% likes");
-    ratioElement.appendChild(ratioText);
-    ThisElement.appendChild(ratioElement);
-})
+        //Make and insert the like-dislike percent as a element
+        var ratioElement = document.createElement("p");
+        var ratioText = document.createTextNode(Math.round(likeDislikePercent)+ "% likes");
+        ratioElement.appendChild(ratioText);
+
+        ThisElement.parentElement.insertBefore(ratioElement,ThisElement.parentElement.childNodes[0] );
+        console.log("ran");
+    })
+}
+
+main();
